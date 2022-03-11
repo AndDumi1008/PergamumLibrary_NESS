ae03dc465a5e68c1543a53a86f697ffd47d6406f
Andrei Dumitrescu <andrei.dumitrescu100801@gmail.com>
2022-03-11 21:19:58 +0200

    Initial commit

---
+++ src/main/java/com/example/pergamumlibrary/Book.java
@@ -0,0 +1,56 @@
+package com.example.pergamumlibrary;
+
+public class Book implements Comparable<Book> {
+    private String title;
+    private String author;
+    private Integer id;
+
+    private static int nextId = 1;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.id = Book.nextId++;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    @Override
+    public int compareTo(Book o) {
+
+        if(this.author.compareTo(o.author) < 0){
+            return -1;
+        }
+        else if(this.author.compareTo(o.author) > 0){
+            return 1;
+        }
+        else{   //when is the same author
+            if(this.title.compareTo(o.title) < 0){
+                return -1;
+            }
+            else if(this.title.compareTo(o.title) > 0) {
+                return 1;
+            }
+            else
+            return 0;
+        }
+    }
+}
